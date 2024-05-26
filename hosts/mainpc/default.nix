3e2577faf063b5622da13f9d610cb9f28be91811
sertchan <ardaseyhantemli1@gmail.com>
2024-05-26 22:53:59 +0300

    hosts/mainpc: remove autoupdate && change nix build options

--- hosts/mainpc/default.nix
+++ hosts/mainpc/default.nix
@@ -75,9 +75,63 @@
 
   nix = {
     settings = {
+      use-xdg-base-directories = true;
+      flake-registry = "/etc/nix/registry.json";
+      min-free = "${toString (5 * 1024 * 1024 * 1024)}";
+      max-free = "${toString (10 * 1024 * 1024 * 1024)}";
       auto-optimise-store = true;
-      experimental-features = [ "nix-command" "flakes" ];
-      trusted-users = [ "root" "@wheel" ];
+      allowed-users = [ "root" "@wheel" "nix-builder" ];
+      trusted-users = [ "root" "@wheel" "nix-builder" ];
+      max-jobs = "auto";
+      sandbox = true;
+      sandbox-fallback = false;
+      system-features = [ "nixos-test" "kvm" "recursive-nix" "big-parallel" ];
+      extra-platforms = config.boot.binfmt.emulatedSystems;
+      keep-going = true;
+      connect-timeout = 5;
+      log-lines = 30;
+      extra-experimental-features = [
+        "flakes"
+        "nix-command"
+        "recursive-nix"
+        "ca-derivations"
+        "auto-allocate-uids"
+        "cgroups"
+      ];
+      warn-dirty = false;
+      http-connections = 50;
+      accept-flake-config = false;
+      use-cgroups = true;
+      keep-derivations = true;
+      keep-outputs = true;
+      builders-use-substitutes = true;
+      substituters = [
+        "https://cache.nixos.org"
+        "https://cache.privatevoid.net"
+        "https://nixpkgs-wayland.cachix.org"
+        "https://nix-community.cachix.org"
+        "https://hyprland.cachix.org"
+        "https://nixpkgs-unfree.cachix.org"
+        "https://numtide.cachix.org"
+      ];
+
+      trusted-public-keys = [
+        "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+        "cache.ngi0.nixos.org-1:KqH5CBLNSyX184S9BKZJo1LxrxJ9ltnY2uAs5c/f1MA="
+        "cache.privatevoid.net:SErQ8bvNWANeAvtsOESUwVYr2VJynfuc9JRwlzTTkVg="
+        "nixpkgs-wayland.cachix.org-1:3lwxaILxMRkVhehr5StQprHdEo4IrE8sRho9R9HOLYA="
+        "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
+        "hyprland.cachix.org-1:a7pgxzMz7+chwVL3/pzj6jIBMioiJM7ypFP8PwtkuGc="
+        "nixpkgs-unfree.cachix.org-1:hqvoInulhbV4nJ9yJOEr+4wxhDV4xq2d1DK7S6Nj6rs="
+        "numtide.cachix.org-1:2ps1kLBUWjxIneOy1Ik6cQjb41X0iXVXeHigGmycPPE="
+        "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+        "notashelf.cachix.org-1:VTTBFNQWbfyLuRzgm2I7AWSDJdqAa11ytLXHBhrprZk="
+        "neovim-flake.cachix.org-1:iyQ6lHFhnB5UkVpxhQqLJbneWBTzM8LBYOFPLNH4qZw="
+        "nyx.cachix.org-1:xH6G0MO9PrpeGe7mHBtj1WbNzmnXr7jId2mCiq6hipE="
+        "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g="
+        "cache.notashelf.dev-1:DhlmJBtURj+XS3j4F8SFFukT8dYgSjtFcd3egH8rE6U="
+        "ags.cachix.org-1:naAvMrz0CuYqeyGNyLgE010iUiuf/qx6kYrUv3NwAJ8="
+      ];
     };
   };
 
@@ -124,37 +178,6 @@
     services = { wg-quick-wg0.wantedBy = lib.mkForce [ ]; };
     user = {
       services = {
-        flake_autoupdate = {
-          description =
-            "Autoupdate flakes (Triggered by flake_autoupdate.timer)";
-          enable = true;
-          path = with pkgs-stable; [
-            bash
-            coreutils
-            git
-            nix
-            nixos-install-tools
-          ];
-          serviceConfig = {
-            ExecStart = pkgs-stable.writeShellScript "update-flakes" ''
-              function update_flakes {
-                nix flake update ~/.nixos
-                cd ~/.nixos || exit
-                git add . && git commit -m "flake.nix: update flakes" && git push
-              }
-
-              function rebuild_system {
-                nixos-rebuild switch --flake ~/.nixos#asli --use-remote-sudo
-                nix-collect-garbage --delete-older-than 21d
-              }
-
-              update_flakes
-              rebuild_system
-            '';
-            Restart = "on-failure";
-            RestartSec = 5;
-          };
-        };
         qbittorrent-nox = {
           description = "Autostarts Qbittorrent-nox";
           enable = true;
@@ -190,17 +213,6 @@
           };
         };
       };
-      timers = {
-        flake_autoupdate = {
-          description = "Autoupdate flakes weekly";
-          wantedBy = [ "timers.target" ];
-          timerConfig = {
-            OnActiveSec = "7d";
-            Persistent = true;
-            Unit = "flake_autoupdate.service";
-          };
-        };
-      };
     };
   };
 
@@ -210,6 +222,11 @@
   programs = {
     hyprland.enable = true;
     zsh.enable = true;
+    nh = {
+      enable = true;
+      package = pkgs.nh;
+      flake = "/home/seyhan/.nixos";
+    };
   };
 
   services = {
