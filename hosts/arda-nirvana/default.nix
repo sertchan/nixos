c24afad08a375b3c5720d53f1a1a3b5387532ddc
sertchan <ardaseyhan@proton.me>
2026-07-19 21:39:34 +0300

    hosts/arda-nirvana: add comments to default.nix

--- hosts/arda-nirvana/default.nix
+++ hosts/arda-nirvana/default.nix
@@ -1,16 +1,22 @@
 {
   imports = [
+    # --- System & Power Profiles ---
     ./system.nix
     ./tlp.nix
 
+    # --- Core Configuration ---
     ../../modules/core
+
+    # --- Hardware Drivers ---
     ../../modules/hardware/intel-gpu.nix
     ../../modules/hardware/bluetooth.nix
 
+    # --- Background Services ---
     ../../modules/services/zapret.nix
     ../../modules/services/dns-content-blocking.nix
     ../../modules/services/qbittorrent-nox.nix
 
+    # --- Desktop ---
     ../../modules/desktop
   ];
 
@@ -18,5 +24,5 @@
 
   time.timeZone = "Europe/Istanbul";
 
-  system.stateVersion = "24.11"; # # DONT CHANGE unless you know what you're doing
+  system.stateVersion = "24.11"; # DONT CHANGE unless you know what you're doing
 }
