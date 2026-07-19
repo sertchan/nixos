ebc711ca1c372e55aa9c248132e3b75436a16c34
sertchan <ardaseyhan@proton.me>
2026-07-19 21:36:52 +0300

    hosts/arda-nirvana: add comments to system.nix

--- hosts/arda-nirvana/system.nix
+++ hosts/arda-nirvana/system.nix
@@ -5,10 +5,12 @@
   ...
 }:
 {
+  # Default hardware scan imports
   imports = [ (modulesPath + "/installer/scan/not-detected.nix") ];
 
   boot = {
     initrd = {
+      # Modules required to detect storage/USB devices during early boot stage
       availableKernelModules = [
         "xhci_pci"
         "ahci"
@@ -20,21 +22,29 @@
       ];
       kernelModules = [ ];
     };
-    kernelModules = [ "kvm-intel" ];
-    kernelParams = [ "mem_sleep_default=deep" ];
+    kernelModules = [ "kvm-intel" ]; # Intel CPU virtualization
+    kernelParams = [ "mem_sleep_default=deep" ]; # Force deep sleep (S3 suspend state)
     extraModulePackages = [ ];
-    tmp.useTmpfs = true;
+    tmp.useTmpfs = true; # Mount /tmp in RAM
   };
+
+  # Enable zram compressed swap space in RAM
   zramSwap.enable = true;
+
   fileSystems = {
+    # Root partition
     "/" = {
       device = "/dev/disk/by-label/NIXROOT";
       fsType = "ext4";
     };
+
+    # EFI system partition
     "/boot" = {
       device = "/dev/disk/by-label/NIXBOOT";
       fsType = "vfat";
     };
+
+    # Screenshot folder in RAM to prevent redundant disk writes
     "/home/seyhan/Pictures/Screenshots" = {
       device = "tmpfs";
       fsType = "tmpfs";
